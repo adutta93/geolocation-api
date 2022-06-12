63cccbd37dc1a9489c55d7798e8b4ca4ba991de0
akash.dutta <dutta.akash1993official@gmail.com>
2022-06-12 20:57:35 +0530

    user schema added

--- src/server.js
+++ src/server.js
@@ -2,11 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const ConnectDb = require('./db/db');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+/**
+ * !MongoDb Connection
+ *
+ */
+ConnectDb();
+
+/**
+ * !Server
+ *
+ */
 const PORT = process.env.PORT || 1993;
 
 app.listen(PORT, () => {
